9d16bc6f1b4f32085773fcf441afc4b34eb36e00
aslaug sollilja <aslaugsollilja@gmail.com>
2016-06-01 13:13:32 -0700

    added a page and route

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -6,7 +6,7 @@ class HomePage extends React.Component {
         return (
             <div className="jumbotron">
                 <h1>Pluralsight Administration</h1>
-                <p> React, Redux and React Router in ES^ for ultra-responsive web apps.</p>
+                <p> React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
