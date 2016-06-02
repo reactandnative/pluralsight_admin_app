77b3c81b6beb943a4a1c92a01297e8b2d84e00ad
aslaug sollilja <aslaugsollilja@gmail.com>
2016-06-02 11:33:21 -0700

    load courses in reducer and dispatch action on page load

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,51 +4,19 @@ import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
-    // In the constructor we initialize state and call our bind functions
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            course: { title:"" }
-        };
-        // Do your bindings in the constructor, not the render!
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    // Our child functions which are called by render ************
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course });
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
         return <div key={index}>{course.title}</div>
     }
-    // **********************************************************
 
-    // For simplicity the markup is inline, usually we would call
-    // child function with the markup
     render() {
         return (
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input
-                    type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-
-                <input
-                    type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} />
             </div>
         );
     }
